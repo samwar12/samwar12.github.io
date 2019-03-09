f1eebc6335b1394b8c6153e6a04f286500137fcd
samwar12 <44182014+samwar12@users.noreply.github.com>
2019-03-10 06:27:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -13,13 +13,7 @@ function playStream(idVideoTag, stream) {
 // openStream()
 // .then(stream => playStream('localStream', stream));
 
-const peer = new Peer({ 
-    key: 'peerjs', 
-    host: 'samwar11.herokuapp.com', 
-    secure: true, 
-    port: 9000, 
-    config: customConfig 
-});
+const peer = new Peer({key: 'lwjd5qra8257b9'});
 
 peer.on('open', id => {
     $('#my-peer').append(id);
