1d7abe104c7938a7709f23eec93bce74fb6f1fca
samwar12 <44182014+samwar12@users.noreply.github.com>
2019-03-10 06:55:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let customConfig;
 $.ajax({
   url: "https://global.xirsys.net",
   data: {
-    ident: "samwar12",
+    ident: "samwar11",
     secret: "bec4837c-40a6-11e9-a62a-0242ac110003",
     domain: "samwar12.github.io",
     application: "default",
