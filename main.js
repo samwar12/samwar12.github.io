05f8ca7e2e09004f541204bf10279e43ac7dc5a8
samwar12 <44182014+samwar12@users.noreply.github.com>
2019-03-09 23:12:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,49 @@
-
 const socket = io('https://samwar11.herokuapp.com/');
+
+$('#div-chat').hide();
+
+let customConfig;
+
+$.ajax({
+  url: "https://service.xirsys.com/ice",
+  data: {
+    ident: "vanpho",
+    secret: "2b1c2dfe-4374-11e7-bd72-5a790223a9ce",
+    domain: "vanpho93.github.io",
+    application: "default",
+    room: "default",
+    secure: 1
+  },
+  success: function (data, status) {
+    // data.d is where the iceServers object lives
+    customConfig = data.d;
+    console.log(customConfig);
+  },
+  async: false
+});
+
+socket.on('DANH_SACH_ONLINE', arrUserInfo => {
+    $('#div-chat').show();
+    $('#div-dang-ky').hide();
+
+    arrUserInfo.forEach(user => {
+        const { ten, peerId } = user;
+        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
+    });
+
+    socket.on('CO_NGUOI_DUNG_MOI', user => {
+        const { ten, peerId } = user;
+        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
+    });
+
+    socket.on('AI_DO_NGAT_KET_NOI', peerId => {
+        $(`#${peerId}`).remove();
+    });
+});
+
+socket.on('DANG_KY_THAT_BAT', () => alert('Vui long chon username khac!'));
+
+
 function openStream() {
     const config = { audio: false, video: true };
     return navigator.mediaDevices.getUserMedia(config);
@@ -18,10 +62,9 @@ const peer = new Peer({
     key: 'peerjs', 
     host: 'samwar11.herokuapp.com', 
     secure: true, 
-    port: 9000, 
+    port: 443, 
     config: customConfig 
 });
-
 
 peer.on('open', id => {
     $('#my-peer').append(id);
