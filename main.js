a6dd29230e64d8d9e0a844d32b75a7cf5764b369
samwar12 <44182014+samwar12@users.noreply.github.com>
2019-03-10 07:36:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,7 @@
 const socket = io('https://glacial-atoll-60287.herokuapp.com/');
 $('#div-chat').hide();
 
-let customConfig;
 
-$.ajax({
-  url: "https://global.xirsys.net",
-  data: {
-    ident: "samwar11",
-    secret: "bec4837c-40a6-11e9-a62a-0242ac110003",
-    domain: "samwar12.github.io",
-    application: "default",
-    room: "default",
-    secure: 1
-  },
-  success: function (data, status) {
-    // data.d is where the iceServers object lives
-    customConfig = data.d;
-    console.log(customConfig);
-  },
-  async: false
-});
-
-socket.on('DANH_SACH_ONLINE', arrUserInfo => {
-    $('#div-chat').show();
-    $('#div-dang-ky').hide();
-
-    arrUserInfo.forEach(user => {
-        const { ten, peerId } = user;
-        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
-    });
-
-    socket.on('CO_NGUOI_DUNG_MOI', user => {
-        const { ten, peerId } = user;
-        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
-    });
-
-    socket.on('AI_DO_NGAT_KET_NOI', peerId => {
-        $(`#${peerId}`).remove();
-    });
-});
-
-socket.on('DANG_KY_THAT_BAT', () => alert('Vui long chon username khac!'));
 
 
 function openStream() {
